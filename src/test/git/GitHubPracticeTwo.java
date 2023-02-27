31da9bcb9169fe644a6112d9994cdd6ab12c2f92
ShamsPa <126110847+ShamsPa@users.noreply.github.com>
2023-02-27 11:45:08 -0800

    Update GitHubPracticeTwo.java
    This is a change from Upstream

--- src/test/git/GitHubPracticeTwo.java
+++ src/test/git/GitHubPracticeTwo.java
@@ -2,9 +2,14 @@ package test.git;
 
 public class GitHubPracticeTwo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public int test(int a, int b) {
+	return a*b;
+	
 
 	}
+	public int test(int x, int y, String message){
+	System.out.println(message);
+	int z = x*y;
+	return z;
 
 }
